f3af57b1590d3e352402cee2d267ae0a3cdd2092
danieltonel <danieltonel.xyz@gmail.com>
2022-01-23 16:11:26 +0100

    add hello world to index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Index() {
   return (
-    <div className="z-50 z-10 container  text-left md:text-center justify-center">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="text-center pt-8">
+      <h1 className="text-3xl font-bold">Hello world!</h1>
     </div>
   );
 }
