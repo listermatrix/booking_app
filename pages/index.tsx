21fbcad698423e44ffbe3b848497bf3cc6b8ea5e
danieltonel <danieltonel.xyz@gmail.com>
2022-01-23 16:12:38 +0100

    headwind format classes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 export default function Index() {
   return (
-    <div className="text-center pt-8">
+    <div className="pt-8 text-center">
       <h1 className="text-3xl font-bold">Hello world!</h1>
     </div>
   );
