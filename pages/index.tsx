9fb432b2d0a296af6afa19dc2910c0ab62100259
danieltonel <danieltonel.xyz@gmail.com>
2022-01-23 16:00:02 +0100

    run prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,7 @@
 export default function Index() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <div className="z-50 z-10 container  text-left md:text-center justify-center">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
-};
+}
