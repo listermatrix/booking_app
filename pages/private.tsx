6274f1846c8d1e2f2d1f452d0b74a4797bb56858
Melchizedek <melchizedek@Melchizedeks-MacBook-Pro.local>
2022-04-15 00:03:40 +0000

    coool

--- pages/private.tsx
+++ pages/private.tsx
@@ -49,7 +49,7 @@ export default function Manage() {
               role="tab"
               aria-controls="tabs-messages"
               aria-selected="false">
-              Messages
+              Cancelled
             </a>
           </li>
         </ul>
@@ -67,16 +67,13 @@ export default function Manage() {
                       <thead className="bg-white border-b">
                         <tr>
                           <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                            #
+                            Date
                           </th>
                           <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                            First
+                            Description
                           </th>
                           <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                            Last
-                          </th>
-                          <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                            Handle
+                            Action
                           </th>
                         </tr>
                       </thead>
@@ -88,32 +85,6 @@ export default function Manage() {
                           </td>
                           <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                             Otto
-                          </td>
-                          <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                            @mdo
-                          </td>
-                        </tr>
-                        <tr className="bg-white border-b">
-                          <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">2</td>
-                          <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                            Jacob
-                          </td>
-                          <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                            Thornton
-                          </td>
-                          <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                            @fat
-                          </td>
-                        </tr>
-                        <tr className="bg-gray-100 border-b">
-                          <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">3</td>
-                          <td
-                            colSpan="2"
-                            className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap text-center">
-                            Larry the Bird
-                          </td>
-                          <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                            @twitter
                           </td>
                         </tr>
                       </tbody>
